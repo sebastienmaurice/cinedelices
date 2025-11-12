5677beeda538c9a8ef3c17a26fb36975f1e0c885
Sébastien Maurice <sebastien.maurice@oclock.school>
2025-11-12 16:32:00 +0100

    Refactor movies page

--- app/controllers/movies.controllers.js
+++ app/controllers/movies.controllers.js
@@ -1,7 +1,6 @@
-import {Movie} from "../models/index.js";
+import { Movie } from "../models/index.js";
 
 const moviesController = {
-
   //Filtrage des films par genre (tous, action, comedie, drame...)
   async filtredMovies(req, res) {
     try {
@@ -9,39 +8,43 @@ const moviesController = {
       const { genre } = req.params;
 
       let movies;
-      if (!genre || genre === "all") {
+      if (!genre || genre === "all" || genre === "tous") {
         movies = await Movie.findAll();
       } else {
         movies = await Movie.findAll({
-          where: {genre: genre},
+          where: { genre: genre },
         });
       }
 
       // Rendu de la vue avec les genres filtrés
       console.log(movies);
-      res.render("movies", { movies, genre });
+      res.render("movies", { movies, selectedGenre: genre || "tous" });
     } catch (error) {
       console.error(error);
       res.status(500).send("pages/error");
     }
   },
-  
 
   // Affichage de la liste des films sur la page des films
   async moviesList(req, res) {
-  try {
-    const movies = await Movie.findAll();
-    res.render("movies", { movies });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("pages/error");
-  }
+    try {
+      const movies = await Movie.findAll();
+      const selectedGenre = req.query.genre || "tous";
+
+      // Si un genre est passé en query, filtrer les films
+      let filteredMovies = movies;
+      if (selectedGenre && selectedGenre !== "tous") {
+        filteredMovies = movies.filter(
+          (movie) => movie.genre === selectedGenre
+        );
+      }
+
+      res.render("movies", { movies: filteredMovies, selectedGenre });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("pages/error");
+    }
   },
-  };
-
-
-
+};
 
 export default moviesController;
-
-
